c8c1f082480039cc731ed454e7d01e6101b357b7
kesmile <kurtkelvisk88@gmail.com>
2015-12-28 16:13:10 -0600

    primer commit

---
+++ db/migrate/20151228202611_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :user
+      t.string :email
+      t.text :password
+      t.boolean :status
+
+      t.timestamps null: false
+    end
+  end
+end
