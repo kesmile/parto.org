31e56108f118c23b743639476951e485b0797af3
kesmile <kurtkelvisk88@gmail.com>
2016-01-06 12:05:20 -0600

    test3

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,9 +11,9 @@ class DashboardController < ApplicationController
   #metodos post y get
   def add_comadrona
       if params[:nombre] != '' && params[:direccion] != '' && params[:telefono]
-        # o = [('a'..'z'), ('1'..'9')].map { |i| i.to_a }.flatten
-        # @string = (1...6).map { o[rand(o.length)] }.join @string
-        user = Comadrona.create(nombre: params[:nombre], direccion: params[:direccion], telefono: params[:telefono], categoria: 'centro05', token: '12345');
+         o = [('a'..'z'), ('1'..'9')].map { |i| i.to_a }.flatten
+         @string = (1...6).map { o[rand(o.length)] }.join @string
+        user = Comadrona.create(nombre: params[:nombre], direccion: params[:direccion], telefono: params[:telefono], categoria: 'centro05', token: @string);
         msg = { :status => "ok", :message => "Se ha guardado exitosamente el token es: <b>" + @string + "</b>", :token => @string}
       else
         msg = { :status => "error", :message => "campos vacios"}
